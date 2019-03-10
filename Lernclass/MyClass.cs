92fa99d6985fa179cab34173038b6a347576ff79
Denis M <denis.m.a.3@gmail.com>
2019-03-10 19:58:31 +0300

    use set and get to private varible

---
+++ Lernclass/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lernclass
+{
+    class MyClass
+    {
+        //примеры 
+        /*public string errMsg;
+        private double bal;
+        private bool isError(byte statys)
+        {
+        }*/
+        //Сравнение доступа к открытым и закрытым членам класса
+        private int alpha; //явно задан спецификатор приват, чтобы не давать доступ непосредственно к переменной, ммоджно создать метод, который бы получал доступ к этой переменной
+        int beta; //private по умолчанию
+        public int gamma;//явно задан спецификатор public
+                         //методы для получения доступа к alfa и beta
+
+        public void SetAlpha(int a)//метол записи значения в приватную перменню
+        {
+            alpha = a;
+        }
+
+        public int GetAlpha()
+        {
+            return alpha;
+        }
+
+    }
+}
--- Lernclass/Program.cs
+++ Lernclass/Program.cs
@@ -10,7 +10,17 @@ namespace Lernclass
     {
         static void Main(string[] args)
         {
-            //hello world
+            //управление доступом осуществляется  public, private, protected, internal
+            //public  разрешает доступ разрешает доступ к члену откуда угодно
+            //private разрешает доступ к члену только для методов внури этого класса
+            //первый пример set, ger к приватной переменной
+
+            MyClass firsttime = new MyClass();
+            //не видит переменнойальфа
+            firsttime.SetAlpha(6);
+            Console.WriteLine(firsttime.GetAlpha());
+             
+
         }
     }
 }
