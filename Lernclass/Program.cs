26064642d491d92e5c5671c0da8ea6c910f49168
Denis M <denis.m.a.3@gmail.com>
2019-03-10 22:14:45 +0300

    Steck and methods

--- Lernclass/Program.cs
+++ Lernclass/Program.cs
@@ -15,11 +15,32 @@ namespace Lernclass
             //private разрешает доступ к члену только для методов внури этого класса
             //первый пример set, ger к приватной переменной
 
-            MyClass firsttime = new MyClass();
-            //не видит переменнойальфа
-            firsttime.SetAlpha(6);
-            Console.WriteLine(firsttime.GetAlpha());
-             
+            /* MyClass firsttime = new MyClass();
+             //не видит переменнойальфа
+             firsttime.SetAlpha(6);
+             Console.WriteLine(firsttime.GetAlpha());
+             Console.ReadKey();*/
+
+            //Создание стека
+            Stack stk1 = new Stack(10);
+            Stack stk2 = new Stack(10);
+            Stack stk3 = new Stack(10);
+            char ch; int i;
+            for (i = 0; !stk1.full(); i++)
+            {
+                stk1.push((char)('A'+i));
+            }
+            if (stk1.full())
+                Console.WriteLine("Стек stk1 полон.");
+            Console.Write("Содержимое стека stk1");
+            while (!stk1.empty())
+            {
+                ch = stk1.pop();
+                Console.Write(ch);
+            }
+            Console.WriteLine();
+            if (stk1.empty()) Console.WriteLine("Стек stk1 пуст.\n");
+            Console.ReadKey();
 
         }
     }
---
+++ Lernclass/Stack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lernclass
+{
+    class Stack
+    {
+        //примеры 
+        /*public string errMsg;
+        private double bal;
+        private bool isError(byte statys)
+        {
+        }*/
+        //Сравнение доступа к открытым и закрытым членам класса
+        /*  private int alpha; //явно задан спецификатор приват, чтобы не давать доступ непосредственно к переменной, ммоджно создать метод, который бы получал доступ к этой переменной
+          int beta; //private по умолчанию
+          public int gamma;//явно задан спецификатор public
+                           //методы для получения доступа к alfa и beta
+
+          public void SetAlpha(int a)//метол записи значения в приватную перменню
+          {
+              alpha = a;
+          }
+
+          public int GetAlpha()
+          {
+              return alpha;
+          }
+          */
+        private char[] stak; //Массив для хранения стека
+        int tos;//индекс вершины стека
+        //создаём пустой класс Stack заданного размера
+        public Stack(int size)
+        {
+            stak = new char[size];
+            tos = 0;
+        }
+        //помешпем символы в стек
+        public void push(char ch)
+        {
+            if (tos == stak.Length)
+            {
+                Console.WriteLine(" - Стэк заполен");
+                return;
+            }
+
+            stak[tos] = ch;
+            tos++;
+        }
+        //извлекаем символы из стека
+        public char pop()
+        {
+            if (tos == 0)
+            {
+                Console.WriteLine(" - Стек пуст.");
+                return (char)0;
+            }
+            tos--;
+            return stak[tos];
+        }
+        //Метод возвращает значение true, если стек полон
+        public bool full()
+        {
+            return tos == stak.Length;
+        }
+        public bool empty()
+        {
+            return tos == 0;
+        }
+        public int capacity()
+        {
+            return stak.Length;
+        }
+        public int getNum()
+        {
+            return tos;
+        }
+
+    }
+}
